aae1e8a634914a6dbd8714000784ede7a5929dd6
eddie-blaze19 <edwarddziworshie@gmail.com>
2023-01-07 02:58:12 +0000

    PY PY PY

---
+++ 0x02-python-import_modules/here.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+chmod u+x variable_load_5.py
+chmod u+x printing.py
+chmod u+x 103-fast_alphabet.py
+chmod u+x 102-magic_calculation.py
+chmod u+x 101-easy_print.py
+chmod u+x 100-my_calculator.py
+chmod u+x 5-variable_load.py
+chmod u+x 4-hidden_discovery.py
+chmod u+x 3-infinite_add.py
+chmod u+x 2-args.py
+chmod u+x 1-calculation.py
+chmod u+x 0-add.py
+chmod u+x add_0.py
+chmod u+x calculator_1.py
+chmod u+x 0-import_add.py
